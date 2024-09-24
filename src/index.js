4f48f910b09c4e5b419a6f8208b25b11704ae934
lowpolytree <minratos@gmail.com>
2024-09-24 15:51:39 -0400

    Added a basic mongo/express server for testing.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-print("App Starting")
+const express = require('express');
+const { MongoClient } = require('mongodb');
+
+const app = express();
+const port = 3000;
+
+// MongoDB connection
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost:27017";
+const client = new MongoClient(mongoUrl);
+
+async function connectDb() {
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error('Failed to connect to MongoDB', err);
+  }
+}
+
+connectDb();
+
+app.get('/', (req, res) => {
+  res.send('URL Shortener is running');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
